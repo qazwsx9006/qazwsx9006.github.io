28b23e88edb4e30d4ffaaa4327f27efffabbe651
TsengMingyu <qazwsx9006@hotmail.com>
2015-01-20 16:57:09 +0800

    test mail

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -62,3 +62,88 @@ function rwd_index(){
 		$('.my_info').toggleClass('rwd_height');
 	});
 }
+
+function mail_check(){
+	$('#submit').click(function(){
+		var $form=$(this).parent('.form'),
+			$subject=$form.find('#subject'),
+			$name=$form.find('#name'),
+			$email=$form.find('#email'),
+			$message=$form.find('#message'),
+			submit=true;
+		if($.trim($subject.val())==''){
+			submit=false;
+			$subject.addClass('err');
+		}else{
+			$subject.removeClass('err');
+		}
+
+		if($.trim($name.val())==''){
+			submit=false;
+			$name.addClass('err');
+		}else{
+			$name.removeClass('err');
+		}
+		
+		if($.trim($email.val())==''){
+			submit=false;
+			$email.addClass('err');
+		}else{
+			$email.removeClass('err');
+		}
+		
+		if($.trim($message.val())==''){
+			submit=false;
+			$message.addClass('err');
+		}else{
+			$message.removeClass('err');
+		}
+
+		if(submit){
+			var message="寄件者："+$name.val()+"。<br\/>"+
+						"信箱："+$email.val()+"。<br\/>"+
+						"內容"+$message.val()+"。<br\/>";
+			var subject=$subject.val();
+
+			$(this).val('傳送中');
+			$(this)[0].disabled=true;
+			$(this).removeClass('submit');
+			var request = $.ajax({
+			    url: "//formspree.io/qazwsx9006@gmail.com", 
+			    method: "POST",
+			    data: {message:message ,_subject:subject },
+			    dataType: "json"
+			});
+			request.done(function( msg ) {
+			  console.log( msg );
+			  $('#submit').val('送出');
+			  $('#submit')[0].disabled=false;
+			  $('#submit').addClass('submit');
+			});
+			 
+			request.fail(function( jqXHR, textStatus ) {
+			  console.log(jqXHR,textStatus);
+			  alert('不好意思，出了點問題');
+			});
+		}
+	});
+}
+
+
+
+function initialize() {
+  var myLatlng = new google.maps.LatLng(25.0172264,121.506378);
+  var mapOptions = {
+    zoom: 10,
+    center: myLatlng
+  }
+  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+  var marker = new google.maps.Marker({
+      position: myLatlng,
+      map: map,
+      title: 'Hello!'
+  });
+  
+}
+google.maps.event.addDomListener(window, 'load', initialize);
